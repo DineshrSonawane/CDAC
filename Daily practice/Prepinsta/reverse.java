8edc258c521032de5d7c0da2a671d6f8e8cd4383
DineshrSonawane <atulsnw@gmail.com>
2024-10-17 00:39:32 +0530

    Add files via upload

---
+++ Daily practice/Prepinsta/reverse.java
@@ -0,0 +1,24 @@
+package prepinsta;
+
+public class reverse {
+
+	
+	    public static void main (String[]args)
+	    {
+
+	        
+	        int num = 1234, reverse = 0, rem;
+
+
+	        
+	        while (num != 0)
+	        {
+	            rem = num % 10;
+	            reverse = reverse * 10 + rem;
+	            num /= 10;
+	        };
+
+	        
+	        System.out.println ("Reversed Number: " + reverse);
+	    }
+	}
